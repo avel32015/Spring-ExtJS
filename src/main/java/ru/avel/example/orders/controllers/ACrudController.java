0bed4c15cb6c0c0d1d4a3accd12c868cf9595e68
a.velikotskiy <a.velikotskiy@akb-it.ru>
2017-12-14 16:57:14 +0500

    Fix

--- src/main/java/ru/avel/example/orders/controllers/ACrudController.java
+++ src/main/java/ru/avel/example/orders/controllers/ACrudController.java
@@ -14,13 +14,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
